4c2a17a3b6f376df120bc3677662d3b36084cc0c
vanessaLatefa <36272786+vanessaLatefa@users.noreply.github.com>
2018-12-20 02:27:47 +0100

    Add files via upload
    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Chat </title>
+	<link  type="text/css" rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+
+  <div id="wrapper">
+	<div id="menu">
+	<p> Welcome user</p>
+	<p> Exit</p>
+	</div>
+	<div id="chatbox"> </div>
+	<form action="" name="message">
+	<input type="text" name="usermsg" id="usermsg" />
+	<input type="submit" name="submitmsg" id="submitmsg" value="Send"/>
+	</form>
+  </div>
+
+<script type="text/javascript" src="ttp://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+	// jQuery Document
+$(document).ready(function(){
+ 
+});
+</script>
+	
+
+
+</body>
+</html>
